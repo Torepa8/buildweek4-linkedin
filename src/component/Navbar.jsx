a6be0f3f8488d890d64f4cd1cfc5509c5a0ee26a
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-11-13 20:58:51 +0100

    bootstrap

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Container, Navbar } from "react-bootstrap";
+
+
+export default function NavBar() {
+    return (
+        <Navbar className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="/img/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    React Bootstrap
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+}
