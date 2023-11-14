afa629344199ceb6e4d5e36b8191d1895716fb55
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-11-14 19:52:00 +0100

    collapse ok

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,14 +1,14 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../img/logo-linkedin.svg"
 import { HouseFill, PeopleFill } from "react-bootstrap-icons";
-import {FaToolbox} from "react-icons/fa"
-import {AiFillMessage} from "react-icons/ai"
-import {IoMdNotifications} from "react-icons/io"
-import {BiUserCircle} from "react-icons/bi"
+import { FaToolbox } from "react-icons/fa"
+import { AiFillMessage } from "react-icons/ai"
+import { IoMdNotifications } from "react-icons/io"
+import { BiUserCircle } from "react-icons/bi"
 
 export default function NavBar() {
     return (
-        <Navbar className="bg-body-tertiary">
+        <Navbar expand="md" className="bg-body-tertiary">
             <Container>
                 {/* <h1>{process.env.REACT_APP_TOKEN}</h1> */}
                 <Navbar.Brand href="#home">
@@ -21,33 +21,34 @@ export default function NavBar() {
                     />{' '}
                     Linkedin
                 </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home" className="d-flex flex-column align-items-center">
-                            <HouseFill className="hicon"/>
+                            <HouseFill className="hicon" />
                             Home
                         </Nav.Link>
                         <Nav.Link href="#link" className="d-flex flex-column align-items-center">
-                            <PeopleFill className="hicon"/>
+                            <PeopleFill className="hicon" />
                             Rete
                         </Nav.Link>
                         <Nav.Link href="#link" className="d-flex flex-column align-items-center">
-                            <FaToolbox className="hicon"/>
+                            <FaToolbox className="hicon" />
                             Lavoro
                         </Nav.Link>
                         <Nav.Link href="#link" className="d-flex flex-column align-items-center">
-                            <AiFillMessage className="hicon"/>
+                            <AiFillMessage className="hicon" />
                             Messagistica
                         </Nav.Link>
                         <Nav.Link href="#link" className="d-flex flex-column align-items-center">
-                            <IoMdNotifications className="hicon"/>
+                            <IoMdNotifications className="hicon" />
                             Notifiche
-                        </Nav.Link>   
+                        </Nav.Link>
                         <Nav.Link href="#link" className="d-flex flex-column align-items-center">
-                            <BiUserCircle className="hicon"/>
+                            <BiUserCircle className="hicon" />
                             Tu
-                        </Nav.Link>                     
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
