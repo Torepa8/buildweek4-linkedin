c5593778ae7141fd739dd2b40c5cf94733cde9e6
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-11-14 17:47:14 +0100

    env e icon navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../img/logo-linkedin.svg"
-import { HouseUp } from "react-bootstrap-icons";
-
+import { HouseFill, PeopleFill } from "react-bootstrap-icons";
+import {FaToolbox} from "react-icons/fa"
+import {AiFillMessage} from "react-icons/ai"
+import {IoMdNotifications} from "react-icons/io"
+import {BiUserCircle} from "react-icons/bi"
 
 export default function NavBar() {
     return (
         <Navbar className="bg-body-tertiary">
             <Container>
+                {/* <h1>{process.env.REACT_APP_TOKEN}</h1> */}
                 <Navbar.Brand href="#home">
                     <img
                         alt=""
@@ -20,21 +24,30 @@ export default function NavBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">
-                            <HouseUp/>
+                        <Nav.Link href="#home" className="d-flex flex-column align-items-center">
+                            <HouseFill className="hicon"/>
+                            Home
                         </Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown> */}
+                        <Nav.Link href="#link" className="d-flex flex-column align-items-center">
+                            <PeopleFill className="hicon"/>
+                            Rete
+                        </Nav.Link>
+                        <Nav.Link href="#link" className="d-flex flex-column align-items-center">
+                            <FaToolbox className="hicon"/>
+                            Lavoro
+                        </Nav.Link>
+                        <Nav.Link href="#link" className="d-flex flex-column align-items-center">
+                            <AiFillMessage className="hicon"/>
+                            Messagistica
+                        </Nav.Link>
+                        <Nav.Link href="#link" className="d-flex flex-column align-items-center">
+                            <IoMdNotifications className="hicon"/>
+                            Notifiche
+                        </Nav.Link>   
+                        <Nav.Link href="#link" className="d-flex flex-column align-items-center">
+                            <BiUserCircle className="hicon"/>
+                            Tu
+                        </Nav.Link>                     
                     </Nav>
                 </Navbar.Collapse>
             </Container>
